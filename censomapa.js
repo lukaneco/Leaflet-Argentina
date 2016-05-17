20ec225e4b94627f19613443393f3d9d56df676f
Alexis Caffa <AlexisCaffa@outlook.com>
2016-05-17 04:56:00 -0300

    no logro mostrar en mapa el geoJson

--- censomapa.js
+++ censomapa.js
@@ -3,16 +3,12 @@ var featureExtents = function (feature) {
     return [{lon: b[0], lat: b[1]}, {lon: b[2], lat: b[3]}];
 };
 
-var loadProvincias = function (e) {
-    for (var i = 0; i < e.features.length; i++) {
-        var feature = e.features[i];
-
-        jQuery.data(feature.element, 'originalClass', 'color' + ((i % 5) + 1));
-        feature.element.setAttribute('class', 'color' + ((i % 5) + 1));
-
-        var name = feature.data.properties.provincia;
-        jQuery.data($("#menu li a:contains('" + name.toUpperCase() + "')")[0], 'feature', feature);
-    }
+var loadProvincia = function (feature, layer) {
+    //jQuery.data(feature.element, 'originalClass', 'color' + ((i % 5) + 1));
+    //feature.element.setAttribute('class', 'color' + ((i % 5) + 1));
+    //
+    var name = feature.properties.provincia;
+    jQuery.data($("#menu li a:contains('" + name.toUpperCase() + "')")[0], 'feature', feature);
 };
 
 var loadDepartamentos = function (e) {
@@ -25,26 +21,16 @@ var loadDepartamentos = function (e) {
 var map = L.map('map_container').setView([51.505, -0.09], 13);
 
 var osm = new L.tileLayer('http://{s}tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 12,
     subdomains: ['a.', 'b.', 'c.', '']
 });
 
 map
-    .setView(new L.LatLng(-29.075, -57.604), 6)
-    .fitBounds([[-39.075, -67.604], [-19.075, -47.604]])
+    .setView(new L.LatLng(-38, -56), 4)
     .addLayer(osm);
 
-$.getJSON("provincias.json", function(geojsonFeature) {
-    L.geoJson(geojsonFeature, {
-        style: function (feature) {
-            return {
-                background: '#000000'
-            };
-        },
-        onEachFeature: this.setProvincia
-    }).addTo(map);
-});
+L.geoJson(provinciasData).addTo(map);
 
 
 
@@ -70,7 +56,7 @@ $.getJSON("provincias.json", function(geojsonFeature) {
 //	.pan("none"));
 
 
-$(document).ready(function () {
+/*$(document).ready(function () {
     $('#menu li a').mouseover(function (e) {
             var f = jQuery.data($(this)[0], 'feature').element;
             f.setAttribute('class', 'provinciaFoco');
@@ -90,7 +76,7 @@ $(document).ready(function () {
 
         });
 
-});
+});*/
 
 
 
